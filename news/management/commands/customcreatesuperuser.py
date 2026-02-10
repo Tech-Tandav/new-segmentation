a984269550b120b92c7d88c715482aaf29fe4816
binaya.parajuli <binaya.parajuli@nicnepal.org>
2026-02-10 22:19:11 +0545

    shell

--- news/management/commands/customcreatesuperuser.py
+++ news/management/commands/customcreatesuperuser.py
@@ -1,21 +1,33 @@
+import os
 from django.core.management.base import BaseCommand
-from django.conf import settings
 from django.contrib.auth import get_user_model
-# from news.models import User
 
+User = get_user_model()
 
 class Command(BaseCommand):
-    help = "Used to create a default superuser."
-    requires_migrations_checks = True
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.User = get_user_model()
-
+    help = "Create superuser from environment variables"
 
     def handle(self, *args, **options):
-        data = {'is_staff': True, 'is_superuser': True, 'is_active':True, "email":"default@gmail.com"}
-        user, _created = self.User.objects.update_or_create(username="tree",
-                                                            defaults=data)
-        user.set_password("tree")
-        user.save()
+        username = "tree"
+        password = "tree"
+
+        if not username or not password:
+            self.stdout.write(
+                self.style.ERROR("Missing environment variables")
+            )
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(
+                self.style.WARNING("Superuser already exists")
+            )
+            return
+
+        User.objects.create_superuser(
+            username=username,
+            password=password
+        )
+
+        self.stdout.write(
+            self.style.SUCCESS("Superuser created successfully")
+        )
